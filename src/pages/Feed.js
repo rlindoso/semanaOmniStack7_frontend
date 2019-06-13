e842e34fcc50f46a2da8952ccfe901a6f6a61d9a
Rafael Lindoso <rafael.lindoso@gmail.com>
2019-06-12 22:32:15 -0300

    Estilos do feed preparado para receber os "posts" do backend

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,9 +1,69 @@
 import React, { Component } from 'react';
 
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component {
     render(){
         return(
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Rafael Lindoso</span>
+                            <span className="place">Recife</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                        <div className="user-info">
+                        </div>
+                    </header>
+
+                    <img src="http://localhost:3333/files/todoroki 1.jpg" alt=""/>
+                    <footer>
+                        <div className="action">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+                        <p>
+                            Um poste legal!
+                            <span>#react #myheroacademia #todoroki</span>
+                        </p>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Rafael Lindoso</span>
+                            <span className="place">Recife</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                        <div className="user-info">
+                        </div>
+                    </header>
+
+                    <img src="http://localhost:3333/files/todoroki 1.jpg" alt=""/>
+                    <footer>
+                        <div className="action">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+                        <p>
+                            Um poste legal!
+                            <span>#react #myheroacademia #todoroki</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
         );
     }
 }
